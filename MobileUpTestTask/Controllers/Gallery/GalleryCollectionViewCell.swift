d8ee9393cc76722f3588593cffa668cff35baeb7
ShokseDev <aleshchenko.daniil@yandex.com>
2022-04-01 04:25:03 +0300

    Fix gallery navBar and add cell

---
+++ MobileUpTestTask/Controllers/Gallery/GalleryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryCollectionViewCell.swift
+//  MobileUpTestTask
+//
+//  Created by Daniil Aleshchenko on 01.04.2022.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
